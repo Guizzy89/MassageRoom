570a89efd0e9b4135a8d308f90786bc5ee1014a9
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-25 19:00:37 +0300

    Не создалась База данных. Для исправления изменил код в файле appsettings.json и в program.cs так же добавил часть кода.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
+using MassageRoom.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Регистрация контекста базы данных
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ??
+                         "Server=(localdb)\\mssqllocaldb;Database=MassageRoomDB;Trusted_Connection=True;")
+);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+// Код конфигурирования middleware и маршрутов
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
