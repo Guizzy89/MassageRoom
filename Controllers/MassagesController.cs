c878a2c0ea447facf3d187cf4eb0d74755b9696a
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-25 20:15:45 +0300

    MassagesController дополняем метождами для полного цикла CRUD

--- Controllers/MassagesController.cs
+++ Controllers/MassagesController.cs
@@ -2,6 +2,7 @@
 using MassageRoom.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace MassageRoom.Controllers;
 
@@ -17,9 +18,106 @@ public class MassagesController : Controller
     // GET: Massages
     public async Task<IActionResult> Index()
     {
-        var services = await _context.Services.ToListAsync();
-        return View(services); // Возвращает представление с перечнем услуг
+        return View(await _context.Services.ToListAsync());
     }
 
-    // Другие методы CRUD (добавлять, редактировать, удалять услуги) реализуются позже
+    // GET: Massages/Details/{id}
+    public async Task<IActionResult> Details(int id)
+    {
+        var service = await _context.Services.FindAsync(id);
+        if (service == null)
+        {
+            return NotFound();
+        }
+        return View(service);
+    }
+
+    // GET: Massages/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Massages/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,Price")] Service service)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(service);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(service);
+    }
+
+    // GET: Massages/Edit/{id}
+    public async Task<IActionResult> Edit(int id)
+    {
+        var service = await _context.Services.FindAsync(id);
+        if (service == null)
+        {
+            return NotFound();
+        }
+        return View(service);
+    }
+
+    // POST: Massages/Edit/{id}
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price")] Service service)
+    {
+        if (id != service.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(service);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Services.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(service);
+    }
+
+    // GET: Massages/Delete/{id}
+    public async Task<IActionResult> Delete(int id)
+    {
+        var service = await _context.Services.FindAsync(id);
+        if (service == null)
+        {
+            return NotFound();
+        }
+        return View(service);
+    }
+
+    // POST: Massages/Delete/{id}
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var service = await _context.Services.FindAsync(id);
+        if (service != null)
+        {
+            _context.Services.Remove(service);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToAction(nameof(Index));
+    }
 }
