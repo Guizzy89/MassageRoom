2e2a11ffe46c5abcbda359b7b9b685525edc2a77
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-25 18:50:00 +0300

    Для контекста Базы Данных создал файл Dataи внутри неё файл ApplicationDbContext.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MassageRoom.Models;
+
+namespace MassageRoom.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Service> Services { get; set; } = null!;
+    public DbSet<Booking> Bookings { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MassageRoomDB;Trusted_Connection=True;");
+    }
+}
